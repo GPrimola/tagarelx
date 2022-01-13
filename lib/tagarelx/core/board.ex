072b067f33c8ffc32cddeefb700dafeae919336f
Gio Lucas Torres <torres.giorgio@icloud.com>
2022-01-13 16:51:28 -0300

    Added game data structure Board

--- lib/tagarelx/core/board.ex
+++ lib/tagarelx/core/board.ex
@@ -2,10 +2,29 @@ defmodule Tagarelx.Core.Board do
   defstruct [:answer, guesses: []]
 
   @type t :: %__MODULE__{}
+  @type guess :: binary()
 
   @total_tries 6
 
   @spec new(answer :: binary()) :: t()
   def new(answer) when is_binary(answer),
     do: %__MODULE__{answer: answer}
+
+  def new(answer) when is_list(answer) and length(answer) == 5,
+    do: %__MODULE__{answer: answer}
+
+  @spec move(t(), guess()) :: t()
+  def move(%{guesses: guesses} = board, guess) when is_binary(guess),
+    do: %{board | guesses: [guess | guesses]}
+
+  @spec won?(t()) :: boolean()
+  def won?(board)
+  def won?(%{guesses: [guess | _rest], answer: answer}), do: guess == answer
+  def won?(_board), do: false
+
+  @spec lost?(t()) :: boolean()
+  def lost?(%{guesses: guesses} = board), do: not won?(board) and length(guesses) == @total_tries
+
+  @spec status(t()) :: %{lost?: boolean(), won?: boolean()}
+  def status(board), do: %{won?: won?(board), lost?: lost?(board)}
 end
--- test/tagarelx/core/board_test.exs
+++ test/tagarelx/core/board_test.exs
@@ -12,4 +12,131 @@ defmodule Tagarelx.Core.BoardTest do
       assert %Board{answer: ^answer, guesses: []} = board
     end
   end
+
+  describe "move/2" do
+    test "should add a guess to board's guesses list when given guess is binary" do
+      answer = "answe"
+      guess = "guess"
+
+      board =
+        Board.new(answer)
+        |> Board.move(guess)
+
+      assert %Board{guesses: [^guess]} = board
+    end
+
+    test "should prepend a guess to board's guesses list when given guess is binary" do
+      answer = "answe"
+      first_guess = "first"
+      second_guess = "secon"
+
+      board =
+        Board.new(answer)
+        |> Board.move(first_guess)
+        |> Board.move(second_guess)
+
+      assert %Board{guesses: [^second_guess, ^first_guess]} = board
+    end
+  end
+
+  describe "won?/1" do
+    test "should return false when board's guesses is empty" do
+      answer = "answe"
+
+      board = Board.new(answer)
+
+      refute Board.won?(board)
+    end
+
+    test "should return false when guess is not like answer" do
+      answer = "answe"
+
+      refute Board.new(answer)
+             |> Board.move("guess")
+             |> Board.won?()
+    end
+
+    test "should return true when guess is like answer" do
+      answer = "answe"
+
+      assert Board.new(answer)
+             |> Board.move("answe")
+             |> Board.won?()
+    end
+  end
+
+  describe "lost?/1" do
+    test "should return false when board's guesses is empty" do
+      answer = "answe"
+
+      board = Board.new(answer)
+
+      refute Board.lost?(board)
+    end
+
+    test "should return false when guess is like answer" do
+      answer = "answe"
+
+      refute Board.new(answer)
+             |> Board.move("answe")
+             |> Board.lost?()
+    end
+
+    test "should return true when guess is not like answer and guesses has 6 tries" do
+      answer = "answe"
+
+      assert Board.new(answer)
+             |> Board.move("one  ")
+             |> Board.move("two  ")
+             |> Board.move("three")
+             |> Board.move("four ")
+             |> Board.move("five ")
+             |> Board.move("six  ")
+             |> Board.lost?()
+    end
+  end
+
+  describe "status/1" do
+    test "should return status of the board when just created" do
+      answer = "answe"
+
+      board_status =
+        Board.new(answer)
+        |> Board.status()
+
+      refute board_status.lost?
+      refute board_status.won?
+    end
+
+    test "should return status of the board when win" do
+      answer = "answe"
+
+      board_status =
+        answer
+        |> Board.new()
+        |> Board.move("answe")
+        |> Board.status()
+
+      refute board_status.lost?
+      assert board_status.won?
+    end
+
+    test "should return status of the board when lost" do
+      answer = "answe"
+
+      board_status =
+        answer
+        |> Board.new()
+        |> Board.move("one  ")
+        |> Board.move("two  ")
+        |> Board.move("three")
+        |> Board.move("four ")
+        |> Board.move("five ")
+        |> Board.move("six  ")
+        |> Board.status()
+
+      assert board_status.lost?
+      refute board_status.won?
+    end
+  end
 end
