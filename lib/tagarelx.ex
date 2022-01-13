a47b1f2329d57c76f026ed5ee384e0427e8adb74
Gio Lucas Torres <torres.giorgio@icloud.com>
2022-01-13 14:46:57 -0300

    First commit

---
+++ lib/tagarelx.ex
@@ -0,0 +1,18 @@
+defmodule Tagarelx do
+  @moduledoc """
+  Documentation for `Tagarelx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tagarelx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tagarelx_test.exs
@@ -0,0 +1,8 @@
+defmodule TagarelxTest do
+  use ExUnit.Case
+  doctest Tagarelx
+
+  test "greets the world" do
+    assert Tagarelx.hello() == :world
+  end
+end
